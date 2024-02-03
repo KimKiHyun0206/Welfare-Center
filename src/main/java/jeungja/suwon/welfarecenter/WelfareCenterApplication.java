2cf1c819b8b36a169e836652356c18209798d3a0
KimKiHyun0206 <rlarlgus0206@naver.com>
2024-02-03 16:37:31 +0900

    init commit

---
+++ src/main/java/jeungja/suwon/welfarecenter/WelfareCenterApplication.java
@@ -0,0 +1,13 @@
+package jeungja.suwon.welfarecenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WelfareCenterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WelfareCenterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/jeungja/suwon/welfarecenter/WelfareCenterApplicationTests.java
@@ -0,0 +1,13 @@
+package jeungja.suwon.welfarecenter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WelfareCenterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
